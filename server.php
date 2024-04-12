adb4a8ad6d6f4464d97d4496b9ba998ba88473db
MassimoRomano <romano93m@gmail.com>
2024-04-12 15:04:50 +0200

    add strlen for paragraph

--- server.php
+++ server.php
@@ -2,5 +2,5 @@
 
 $paragrafo=$_GET['paragrafo'];
 
-echo "<h2>Paragrafo:</h2>". ' ' . "<h3>$paragrafo</h3>";
+echo "<h2>Paragrafo:</h2>". ' ' . "<h3>$paragrafo</h3>" . '' . "<p>Lunghezza Paragrafo:" .strlen($paragrafo)."</p>" ;
 
