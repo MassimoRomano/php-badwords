fe173ccb5b4c71619c180921d34718ea768c381d
MassimoRomano <romano93m@gmail.com>
2024-04-12 15:18:15 +0200

    add censura

--- server.php
+++ server.php
@@ -1,6 +1,11 @@
 <?php
 
 $paragrafo=$_GET['paragrafo'];
+$censura=$_GET['censura'];
 
 echo "<h2>Paragrafo:</h2>". ' ' . "<h3>$paragrafo</h3>" . '' . "<p>Lunghezza Paragrafo:" .strlen($paragrafo)."</p>" ;
+
+$censura= str_ireplace("$censura",'***',"$paragrafo");
+
+echo "<h2>Censura:</h2>". ' ' ."<h3>$censura</h3>";
 
